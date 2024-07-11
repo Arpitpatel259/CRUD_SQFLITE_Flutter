5a49d4ef142031729fc1338e7808ae6b7bc3f70f
Arpit Vekariya <arpit.vekariya123@gmail,com>
2024-07-11 10:41:32 +0530

    Commit

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -2,7 +2,7 @@ import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
 
-  /// Table Create Function
+  /// Table Create Func
   static Future<void> createTables(sql.Database database) async {
     await database.execute("""CREATE TABLE data(
       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -12,6 +12,7 @@ class SQLHelper {
       );""");
   }
 
+  // Database Open Func
   static Future<sql.Database> db() async {
     return sql.openDatabase("database_name.db", version: 1,
         onCreate: (sql.Database database, int version) async {
@@ -19,8 +20,8 @@ class SQLHelper {
         });
   }
 
-  // Data Create
-  static Future<int> createData(String title, String? desc) async {
+  // Data Insert
+  static Future<int> insertData(String title, String? desc) async {
     final db = await SQLHelper.db();
 
     final data = {'title': title, 'desc': desc};
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,8 +9,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Map<String, dynamic>> _allData = [];
   bool _isLoading = true;
+  List<Map<String, dynamic>> _allData = [];
+
+  // Value Controller
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
 
   /// Get All Data
   void _refreshData() async {
@@ -21,15 +31,9 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _refreshData();
-  }
-
   /// Add Data
   Future<void> _addData() async {
-    await SQLHelper.createData(_titleController.text, _descController.text);
+    await SQLHelper.insertData(_titleController.text, _descController.text);
     _refreshData();
   }
 
@@ -42,20 +46,21 @@ class _HomeScreenState extends State<HomeScreen> {
   /// Delete Data
   void _deleteData(int id) async {
     await SQLHelper.deleteData(id);
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (!mounted) return;
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       backgroundColor: Colors.redAccent,
-      content: Text("Data Deleted"),
+      content: Text("Data Deleted with id no : ${id.toString()}"),
     ));
     _refreshData();
   }
 
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descController = TextEditingController();
-
   void showBottomSheet(int? id) async {
     if (id != null) {
       final existingData =
-      _allData.firstWhere((element) => element['id'] == id);
+          _allData.firstWhere((element) => element['id'] == id);
       _titleController.text = existingData['title'];
       _descController.text = existingData['desc'];
     }
@@ -65,66 +70,67 @@ class _HomeScreenState extends State<HomeScreen> {
         isScrollControlled: true,
         context: context,
         builder: (_) => Container(
-          padding: EdgeInsets.only(
-              top: 30,
-              left: 15,
-              right: 15,
-              bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextField(
-                controller: _titleController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Title",
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: _descController,
-                maxLines: 4,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Description",
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (id == null) {
-                      await _addData();
-                    }
-                    if (id != null) {
-                      await _updateData(id);
-                    }
+              padding: EdgeInsets.only(
+                  top: 30,
+                  left: 15,
+                  right: 15,
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Title",
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: _descController,
+                    maxLines: 4,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Description",
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (id == null) {
+                          await _addData();
+                        }
+                        if (id != null) {
+                          await _updateData(id);
+                        }
 
-                    _titleController.text = "";
-                    _descController.text = "";
+                        _titleController.text = "";
+                        _descController.text = "";
 
-                    Navigator.of(context).pop();
-                    debugPrint("Data data ");
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(18),
-                    child: Text(
-                      id == null ? "Add Data" : "Update Data",
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
+                        Navigator.of(context).pop();
+                        debugPrint("Data data ");
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(18),
+                        child: Text(
+                          id == null ? "Add Data" : "Update Data",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                  const SizedBox(height: 20),
+                ],
               ),
-              const SizedBox(height: 20),
-            ],
-          ),
-        ));
+            ));
   }
 
+  // Basic Loading View
   Widget _buildSkeletonLoader() {
     return ListView.builder(
       itemCount: 5, // Number of skeleton items to show
@@ -152,54 +158,69 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: const Color(0xFFECEAF4),
       appBar: AppBar(
-        title: const Text("CRUD Operation"),
-      ),
-      body: _isLoading
-          ? _buildSkeletonLoader()
-          : ListView.builder(
-        itemCount: _allData.length,
-        itemBuilder: (context, index) => Card(
-          margin: const EdgeInsets.all(15),
-          child: ListTile(
-            title: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: Text(
-                _allData[index]['title'],
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            subtitle: Text(_allData[index]['desc']),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    showBottomSheet(_allData[index]['id']);
-                  },
-                  icon: const Icon(
-                    Icons.edit,
-                    color: Colors.indigo,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    _deleteData(_allData[index]['id']);
-                  },
-                  icon: const Icon(
-                    Icons.delete,
-                    color: Colors.redAccent,
-                  ),
-                ),
-              ],
-            ),
-          ),
+        title: const Text(
+          "CRUD Operation",
+          style: TextStyle(color: Colors.white),
         ),
+        backgroundColor: Colors.black87,
       ),
+      body:
+          _isLoading // if it is true than called Skeleton else called Listview.
+              ? _buildSkeletonLoader()
+              : _allData.isEmpty
+                  ? const Center(
+                      child: Text("No Data Found"),
+                    )
+                  : ListView.builder(
+                      itemCount: _allData.length,
+                      itemBuilder: (context, index) => Card(
+                        margin: const EdgeInsets.all(15),
+                        child: ListTile(
+                          title: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Text(
+                              _allData[index]['title'],
+                              style: const TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          subtitle: Text(_allData[index]['desc'] +
+                              "\n" +
+                              _allData[index]['createdAt']),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  showBottomSheet(_allData[index]['id']);
+                                },
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: Colors.indigo,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  _deleteData(_allData[index]['id']);
+                                },
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.redAccent,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
         onPressed: () => showBottomSheet(null),
-        child: const Icon(Icons.add),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
